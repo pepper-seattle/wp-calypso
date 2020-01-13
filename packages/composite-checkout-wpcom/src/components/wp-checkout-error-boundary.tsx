f106f9fc4cd88552f3c88c414000e4e3848f5f09
nbloomf <nathan.bloomfield@automattic.com>
2020-01-13 12:03:13 -0600

    Translate error string in WpcomCheckoutErrorBoundary

--- packages/composite-checkout-wpcom/src/components/wp-checkout-error-boundary.tsx
+++ packages/composite-checkout-wpcom/src/components/wp-checkout-error-boundary.tsx
@@ -4,32 +4,32 @@
 import React from 'react';
 
 type WPCheckoutErrorBoundaryState = {
-	componentTitle: string;
 	hasError: boolean;
 	errorMessage: string;
 };
 
-const initialWPCheckoutErrorBoundaryState = title => {
-	return {
-		componentTitle: title,
-		errorMessage: '',
-		hasError: false,
-	};
+const initialWPCheckoutErrorBoundaryState = {
+	errorMessage: '',
+	hasError: false,
 };
 
 export default class WpcomCheckoutErrorBoundary extends React.Component {
+	state = initialWPCheckoutErrorBoundaryState;
+
 	constructor( props ) {
 		super( props );
-		this.state = initialWPCheckoutErrorBoundaryState( props.componentTitle );
 	}
 
-	static getDerivedStateFromError() {
-		return { hasError: true };
+	static getDerivedStateFromError( error ): WPCheckoutErrorBoundaryState {
+		return {
+			hasError: true,
+			errorMessage: error,
+		};
 	}
 
 	render() {
 		if ( this.state.hasError ) {
-			return <h1>{ `Something went wrong in ${ this.state.componentTitle }.` }</h1>;
+			return <h1>{ `Something went wrong in composite-checkout-wpcom.` }</h1>;
 		}
 
 		return this.props.children;
